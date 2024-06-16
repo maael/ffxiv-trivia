ad2e60320da007a48398c288efe2d2d2699465f8
Matt Elphick <matt.a.elphy@gmail.com>
2024-06-16 13:17:09 +0100

    fix: Improve header styling on mobile

--- src/components/primitives/Header.tsx
+++ src/components/primitives/Header.tsx
@@ -9,6 +9,7 @@ import cls from 'classnames'
 export default function Header() {
   const { data: session } = useSession()
   const userStyles = getUserStyles(session?.user?.name, (session?.user as any)?.style)
+  const isAdmin = ADMINS.includes(`${session?.user?.name}`)
   return (
     <div>
       <div className="max-w-6xl w-full overflow-hidden mx-auto h-12 px-1 py-2 flex flex-row gap-2 sm:gap-5 items-center text-white">
@@ -36,16 +37,17 @@ export default function Header() {
                 height={20}
                 className={cls('rounded-full', userStyles.border)}
               />
-              <span className={cls('text-ellipsis whitespace-nowrap overflow-hidden', userStyles.text)}>
+              <span className={cls('hidden sm:block text-ellipsis whitespace-nowrap overflow-hidden', userStyles.text)}>
                 {(session.user as any)?.lodestoneData?.name || session.user?.name}
               </span>
             </div>
           </Link>
         ) : null}
-        {ADMINS.includes(`${session?.user?.name}`) ? (
+        {isAdmin ? (
           <Link href={`/questions`}>
             <div className="cursor-link font-trajan flex flex-row gap-2 justify-center items-center bg-brown-brushed rounded-full px-4 py-1 hover:scale-110 transition-transform drop-shadow-lg h-full">
-              <FaPencilAlt /> Edit
+              <FaPencilAlt />
+              <span className="hidden sm:block">Edit</span>
             </div>
           </Link>
         ) : null}
